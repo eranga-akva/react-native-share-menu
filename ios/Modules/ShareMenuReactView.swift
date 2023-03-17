5e0031292f90eca929bed70899033e5498a3713b
erangad <erangadulshan.14@cse.mrt.ac.lk>
2023-03-17 12:34:07 +0530

    roll back to 5.0.5

--- ios/Modules/ShareMenuReactView.swift
+++ ios/Modules/ShareMenuReactView.swift
@@ -3,9 +3,8 @@
 //  RNShareMenu
 //
 //  Created by Gustavo Parreira on 28/07/2020.
-//  Modified by Veselin Stoyanov on 17/04/2021.
+//
 
-import Foundation
 import MobileCoreServices
 
 @objc(ShareMenuReactView)
@@ -66,12 +65,12 @@ public class ShareMenuReactView: NSObject {
 
         let extensionContext = viewDelegate.loadExtensionContext()
 
-        guard let items = extensionContext.inputItems as? [NSExtensionItem] else {
-            print("Error: \(COULD_NOT_FIND_ITEMS_ERROR)")
+        guard let item = extensionContext.inputItems.first as? NSExtensionItem else {
+            print("Error: \(COULD_NOT_FIND_ITEM_ERROR)")
             return
         }
 
-        viewDelegate.continueInApp(with: items, and: extraData)
+        viewDelegate.continueInApp(with: item, and: extraData)
     }
 
     @objc(data:reject:)
@@ -83,96 +82,91 @@ public class ShareMenuReactView: NSObject {
             return
         }
 
-        extractDataFromContext(context: extensionContext) { (data, error) in
+        extractDataFromContext(context: extensionContext) { (data, mimeType, error) in
             guard (error == nil) else {
                 reject("error", error?.description, nil)
                 return
             }
 
-            resolve([DATA_KEY: data])
+            resolve([MIME_TYPE_KEY: mimeType, DATA_KEY: data])
         }
     }
 
-    func extractDataFromContext(context: NSExtensionContext, withCallback callback: @escaping ([Any]?, NSException?) -> Void) {
-        DispatchQueue.global().async {
-            let semaphore = DispatchSemaphore(value: 0)
-            let items:[NSExtensionItem]! = context.inputItems as? [NSExtensionItem]
-            var results: [[String: String]] = []
+    func extractDataFromContext(context: NSExtensionContext, withCallback callback: @escaping (String?, String?, NSException?) -> Void) {
+        let item:NSExtensionItem! = context.inputItems.first as? NSExtensionItem
+        let attachments:[AnyObject]! = item.attachments
 
-            for item in items {
-                guard let attachments = item.attachments else {
-                    callback(nil, NSException(name: NSExceptionName(rawValue: "Error"), reason:"couldn't find attachments", userInfo:nil))
-                    return
-                }
+        var urlProvider:NSItemProvider! = nil
+        var imageProvider:NSItemProvider! = nil
+        var textProvider:NSItemProvider! = nil
+        var dataProvider:NSItemProvider! = nil
 
-                for provider in attachments {
-                    if provider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
-                        provider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil) { (item, error) in
-                            let url: URL! = item as? URL
+        for provider in attachments {
+            if provider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
+                urlProvider = provider as? NSItemProvider
+                break
+            } else if provider.hasItemConformingToTypeIdentifier(kUTTypeText as String) {
+                textProvider = provider as? NSItemProvider
+                break
+            } else if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
+                imageProvider = provider as? NSItemProvider
+                break
+            } else if provider.hasItemConformingToTypeIdentifier(kUTTypeData as String) {
+                dataProvider = provider as? NSItemProvider
+                break
+            }
+        }
 
-                            results.append([DATA_KEY: url.absoluteString, MIME_TYPE_KEY: "text/plain"])
+        if (urlProvider != nil) {
+            urlProvider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil) { (item, error) in
+                let url: URL! = item as? URL
 
-                            semaphore.signal()
+                callback(url.absoluteString, "text/plain", nil)
+            }
+        } else if (imageProvider != nil) {
+            imageProvider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil) { (item, error) in
+                let imageUrl: URL! = item as? URL
+
+                if (imageUrl != nil) {
+                    if let imageData = try? Data(contentsOf: imageUrl) {
+                        callback(imageUrl.absoluteString, self.extractMimeType(from: imageUrl), nil)
+                    }
+                } else {
+                    let image: UIImage! = item as? UIImage
+
+                    if (image != nil) {
+                        let imageData: Data! = image.pngData();
+
+                        // Creating temporary URL for image data (UIImage)
+                        guard let imageURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("TemporaryScreenshot.png") else {
+                            return
                         }
-                        semaphore.wait()
-                    } else if provider.hasItemConformingToTypeIdentifier(kUTTypeText as String) {
-                        provider.loadItem(forTypeIdentifier: kUTTypeText as String, options: nil) { (item, error) in
-                            let text:String! = item as? String
 
-                            results.append([DATA_KEY: text, MIME_TYPE_KEY: "text/plain"])
+                        do {
+                            // Writing the image to the URL
+                            try imageData.write(to: imageURL)
 
-                            semaphore.signal()
+                            callback(imageURL.absoluteString, imageURL.extractMimeType(), nil)
+                        } catch {
+                            callback(nil, nil, NSException(name: NSExceptionName(rawValue: "Error"), reason:"Can't load image", userInfo:nil))
                         }
-                        semaphore.wait()
-                    } else if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-                        provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil) { (item, error) in
-                            let imageUrl: URL! = item as? URL
-
-                            if (imageUrl != nil) {
-                                if let imageData = try? Data(contentsOf: imageUrl) {
-                                    results.append([DATA_KEY: imageUrl.absoluteString, MIME_TYPE_KEY: self.extractMimeType(from: imageUrl)])
-                                }
-                            } else {
-                                let image: UIImage! = item as? UIImage
-
-                                if (image != nil) {
-                                    let imageData: Data! = image.pngData();
-
-                                    // Creating temporary URL for image data (UIImage)
-                                    guard let imageURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("TemporaryScreenshot.png") else {
-                                        return
-                                    }
-
-                                    do {
-                                        // Writing the image to the URL
-                                        try imageData.write(to: imageURL)
-
-                                        results.append([DATA_KEY: imageUrl.absoluteString, MIME_TYPE_KEY: imageURL.extractMimeType()])
-                                    } catch {
-                                        callback(nil, NSException(name: NSExceptionName(rawValue: "Error"), reason:"Can't load image", userInfo:nil))
-                                    }
-                                }
-                            }
-
-                            semaphore.signal()
-                        }
-                        semaphore.wait()
-                    } else if provider.hasItemConformingToTypeIdentifier(kUTTypeData as String) {
-                        provider.loadItem(forTypeIdentifier: kUTTypeData as String, options: nil) { (item, error) in
-                            let url: URL! = item as? URL
-
-                            results.append([DATA_KEY: url.absoluteString, MIME_TYPE_KEY: self.extractMimeType(from: url)])
-
-                            semaphore.signal()
-                        }
-                        semaphore.wait()
-                    } else {
-                        callback(nil, NSException(name: NSExceptionName(rawValue: "Error"), reason:"couldn't find provider", userInfo:nil))
                     }
                 }
             }
+        } else if (textProvider != nil) {
+            textProvider.loadItem(forTypeIdentifier: kUTTypeText as String, options: nil) { (item, error) in
+                let text:String! = item as? String
 
-            callback(results, nil)
+                callback(text, "text/plain", nil)
+            }
+        }  else if (dataProvider != nil) {
+            dataProvider.loadItem(forTypeIdentifier: kUTTypeData as String, options: nil) { (item, error) in
+                let url: URL! = item as? URL
+
+                callback(url.absoluteString, self.extractMimeType(from: url), nil)
+            }
+        } else {
+            callback(nil, nil, NSException(name: NSExceptionName(rawValue: "Error"), reason:"couldn't find provider", userInfo:nil))
         }
     }
 
--- ios/ShareViewController.swift
+++ ios/ShareViewController.swift
@@ -6,9 +6,7 @@
 //
 //  Created by Gustavo Parreira on 26/07/2020.
 //
-//  Modified by Veselin Stoyanov on 17/04/2021.
 
-import Foundation
 import MobileCoreServices
 import UIKit
 import Social
@@ -17,7 +15,6 @@ import RNShareMenu
 class ShareViewController: SLComposeServiceViewController {
   var hostAppId: String?
   var hostAppUrlScheme: String?
-  var sharedItems: [Any] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -42,12 +39,12 @@ class ShareViewController: SLComposeServiceViewController {
 
     override func didSelectPost() {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-      guard let items = extensionContext?.inputItems as? [NSExtensionItem] else {
+      guard let item = extensionContext?.inputItems.first as? NSExtensionItem else {
         cancelRequest()
         return
       }
 
-      handlePost(items)
+      handlePost(item)
     }
 
     override func configurationItems() -> [Any]! {
@@ -55,50 +52,24 @@ class ShareViewController: SLComposeServiceViewController {
         return []
     }
 
-  func handlePost(_ items: [NSExtensionItem], extraData: [String:Any]? = nil) {
-    DispatchQueue.global().async {
-      guard let hostAppId = self.hostAppId else {
-        self.exit(withError: NO_INFO_PLIST_INDENTIFIER_ERROR)
-        return
-      }
-      guard let userDefaults = UserDefaults(suiteName: "group.\(hostAppId)") else {
-        self.exit(withError: NO_APP_GROUP_ERROR)
-        return
-      }
+  func handlePost(_ item: NSExtensionItem, extraData: [String:Any]? = nil) {
+    guard let provider = item.attachments?.first else {
+      cancelRequest()
+      return
+    }
 
-      if let data = extraData {
-        self.storeExtraData(data)
-      } else {
-        self.removeExtraData()
-      }
+    if let data = extraData {
+      storeExtraData(data)
+    } else {
+      removeExtraData()
+    }
 
-      let semaphore = DispatchSemaphore(value: 0)
-      var results: [Any] = []
-
-      for item in items {
-        guard let attachments = item.attachments else {
-          self.cancelRequest()
-          return
-        }
-
-        for provider in attachments {
-          if provider.isText {
-            self.storeText(withProvider: provider, semaphore)
-          } else if provider.isURL {
-            self.storeUrl(withProvider: provider, semaphore)
-          } else {
-            self.storeFile(withProvider: provider, semaphore)
-          }
-
-          semaphore.wait()
-        }
-      }
-
-      userDefaults.set(self.sharedItems,
-                       forKey: USER_DEFAULTS_KEY)
-      userDefaults.synchronize()
-
-      self.openHostApp()
+    if provider.isText {
+      storeText(withProvider: provider)
+    } else if provider.isURL {
+      storeUrl(withProvider: provider)
+    } else {
+      storeFile(withProvider: provider)
     }
   }
 
@@ -128,7 +99,7 @@ class ShareViewController: SLComposeServiceViewController {
     userDefaults.synchronize()
   }
   
-  func storeText(withProvider provider: NSItemProvider, _ semaphore: DispatchSemaphore) {
+  func storeText(withProvider provider: NSItemProvider) {
     provider.loadItem(forTypeIdentifier: kUTTypeText as String, options: nil) { (data, error) in
       guard (error == nil) else {
         self.exit(withError: error.debugDescription)
@@ -138,13 +109,24 @@ class ShareViewController: SLComposeServiceViewController {
         self.exit(withError: COULD_NOT_FIND_STRING_ERROR)
         return
       }
+      guard let hostAppId = self.hostAppId else {
+        self.exit(withError: NO_INFO_PLIST_INDENTIFIER_ERROR)
+        return
+      }
+      guard let userDefaults = UserDefaults(suiteName: "group.\(hostAppId)") else {
+        self.exit(withError: NO_APP_GROUP_ERROR)
+        return
+      }
       
-      self.sharedItems.append([DATA_KEY: text, MIME_TYPE_KEY: "text/plain"])
-      semaphore.signal()
+      userDefaults.set([DATA_KEY: text, MIME_TYPE_KEY: "text/plain"],
+                       forKey: USER_DEFAULTS_KEY)
+      userDefaults.synchronize()
+      
+      self.openHostApp()
     }
   }
   
-  func storeUrl(withProvider provider: NSItemProvider, _ semaphore: DispatchSemaphore) {
+  func storeUrl(withProvider provider: NSItemProvider) {
     provider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil) { (data, error) in
       guard (error == nil) else {
         self.exit(withError: error.debugDescription)
@@ -154,13 +136,24 @@ class ShareViewController: SLComposeServiceViewController {
         self.exit(withError: COULD_NOT_FIND_URL_ERROR)
         return
       }
+      guard let hostAppId = self.hostAppId else {
+        self.exit(withError: NO_INFO_PLIST_INDENTIFIER_ERROR)
+        return
+      }
+      guard let userDefaults = UserDefaults(suiteName: "group.\(hostAppId)") else {
+        self.exit(withError: NO_APP_GROUP_ERROR)
+        return
+      }
       
-      self.sharedItems.append([DATA_KEY: url.absoluteString, MIME_TYPE_KEY: "text/plain"])
-      semaphore.signal()
+      userDefaults.set([DATA_KEY: url.absoluteString, MIME_TYPE_KEY: "text/plain"],
+                       forKey: USER_DEFAULTS_KEY)
+      userDefaults.synchronize()
+      
+      self.openHostApp()
     }
   }
   
-  func storeFile(withProvider provider: NSItemProvider, _ semaphore: DispatchSemaphore) {
+  func storeFile(withProvider provider: NSItemProvider) {
     provider.loadItem(forTypeIdentifier: kUTTypeData as String, options: nil) { (data, error) in
       guard (error == nil) else {
         self.exit(withError: error.debugDescription)
@@ -172,6 +165,10 @@ class ShareViewController: SLComposeServiceViewController {
       }
       guard let hostAppId = self.hostAppId else {
         self.exit(withError: NO_INFO_PLIST_INDENTIFIER_ERROR)
+        return
+      }
+      guard let userDefaults = UserDefaults(suiteName: "group.\(hostAppId)") else {
+        self.exit(withError: NO_APP_GROUP_ERROR)
         return
       }
       guard let groupFileManagerContainer = FileManager.default
@@ -192,8 +189,11 @@ class ShareViewController: SLComposeServiceViewController {
         return
       }
       
-      self.sharedItems.append([DATA_KEY: filePath.absoluteString, MIME_TYPE_KEY: mimeType])
-      semaphore.signal()
+      userDefaults.set([DATA_KEY: filePath.absoluteString,  MIME_TYPE_KEY: mimeType],
+                       forKey: USER_DEFAULTS_KEY)
+      userDefaults.synchronize()
+      
+      self.openHostApp()
     }
   }
 
